f235207f67f6e71d710f303d6a1117319ceb9bfc
jorgemarpa <jorgemarpa@ug.uchile.cl>
2019-08-28 17:27:28 +0100

    tab with sliders working at minimum capacity

--- scripts/sliders.py
+++ scripts/sliders.py
@@ -2,9 +2,10 @@ import numpy as np
 
 from bokeh.io import curdoc
 from bokeh.layouts import row, column, WidgetBox
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Panel
 from bokeh.models.widgets import Slider, TextInput
 from bokeh.plotting import figure
+import torch
 
 # Set up data
 N = 200
@@ -17,11 +18,18 @@ source = ColumnDataSource(data=dict(x=x, y=y))
 
 def sliders_tab(model, latent_dim=10, data_type='ts'):
 
-    def eval_decoder(latent_vector):
+    def eval_decoder(latent_vector, cls=None):
 
-        return model.decoder(latent_vector)
+        latent_vector = torch.from_numpy(latent_vector.reshape(1,-1))
+        print(latent_vector)
+        xhat = model.decoder(latent_vector).detach().numpy()
 
-    def make_plot(lcs, cls=''):
+        data = ColumnDataSource(data = {'x': xhat[0,:,0],
+                                        'y': xhat[0,:,1],
+                                        'y_e': xhat[0,:,2]})
+        return data
+
+    def make_plot(src, cls=''):
 
         if data_type == 'ts':
             titel = 'Time Series'
@@ -44,18 +52,20 @@ def sliders_tab(model, latent_dim=10, data_type='ts'):
                    x_axis_label=xlabel,
                    y_axis_label=ylabel)
 
-        real = p.circle(lc[0,:,0], lc[0,:,1], color='royalblue',
-                        size=5, line_alpha=0)
+        p.circle('x', 'y', source=src, color='royalblue',
+                 size=5, line_alpha=0)
 
         return p
 
     def update(attr, old, new):
 
-        xhat = eval_decoder(latent_vector)
+        latent_vector = np.zeros(latent_dim, dtype=np.float32)
+        for i, sld in sliders.items():
+            latent_vector[i] = sld.value
 
-        real.data_source.data['x'] = xhat[0,:,0]
-        real.data_source.data['y'] = xhat[0,:,1]
+        new_src = eval_decoder(latent_vector)
 
+        src.data.update(new_src.data)
 
     # Set up widgets
     # if data_type == 'ts':
@@ -63,21 +73,24 @@ def sliders_tab(model, latent_dim=10, data_type='ts'):
     #                    options=['RRL','LPV','Cep','EB','dSct'])
     sliders = {}
     for nn in range(latent_dim):
-        sliders[nn] = Slider(title="Dim %i" % (nn),
-                            value=0,
-                            start=-5.0,
-                            end=5.0,
-                            step=0.1)
+        sliders[nn] = Slider(title="Dim %i" % (nn+1),
+                             value=0,
+                             start=-5.0,
+                             end=5.0,
+                             step=0.1)
         sliders[nn].on_change('value', update)
 
-    src = eval_decoder(np.random.normal(0, .1, size=latent_dim))
+    src = eval_decoder(np.zeros(latent_dim).astype(np.float32))
 
     p = make_plot(src)
 
     # Put controls in a single element
-    controls = WidgetBox(sliders.items())
+    sld_list = list(sliders.values())
+    controls = column(sld_list)
 
-    layers = row(controls, p)
+    layout = row(controls, p)
 
     # Make a tab with the layout
     tab = Panel(child=layout, title='Sliders')
+
+    return tab
